ce52f0b08b3118da16982474d8cac5fbf1004dc9
Naratip Udomsilp <naratip.ud@gmail.com>
2018-01-04 11:20:12 +0700

    step 15: creating a pizza via dispatch, effects, reducers

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -24,4 +24,17 @@ export class PizzasEffect {
         );
     })
   );
+
+  @Effect()
+  createPizza$ = this.actions$.ofType(pizzaActions.CREATE_PIZZA).pipe(
+    map((action: pizzaActions.CreatePizza) => action.payload),
+    switchMap(pizza => {
+      return this.pizzaService
+        .createPizza(pizza)
+        .pipe(
+          map(pizza => new pizzaActions.CreatePizzaSuccess(pizza)),
+          catchError(error => of(new pizzaActions.CreatePizzaFail(error)))
+        );
+    })
+  );
 }
