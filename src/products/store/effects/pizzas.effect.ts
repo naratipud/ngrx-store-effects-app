5bb7b261718834600cc3d54567d17882550de5d4
Naratip Udomsilp <naratip.ud@gmail.com>
2018-01-04 16:31:52 +0700

    step 19: dispatch routing actions, combine ofType for single effect

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -3,6 +3,7 @@ import { Actions, Effect } from '@ngrx/effects';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, switchMap } from 'rxjs/operators';
 
+import * as fromRoot from '../../../app/store';
 import * as fromServices from '../../services';
 import * as pizzaActions from '../actions/pizzas.action';
 
@@ -39,6 +40,16 @@ export class PizzasEffect {
   );
 
   @Effect()
+  createPizzaSuccess$ = this.actions$
+    .ofType(pizzaActions.CREATE_PIZZA_SUCCESS)
+    .pipe(
+      map((action: pizzaActions.CreatePizzaSuccess) => action.payload),
+      map(pizza => {
+        return new fromRoot.Go({ path: ['products', pizza.id] });
+      })
+    );
+
+  @Effect()
   updatePizza$ = this.actions$.ofType(pizzaActions.UPDATE_PIZZA).pipe(
     map((action: pizzaActions.UpdatePizza) => action.payload),
     switchMap(pizza => {
@@ -63,4 +74,16 @@ export class PizzasEffect {
         );
     })
   );
+
+  @Effect()
+  handlePizzaSuccess$ = this.actions$
+    .ofType(
+      pizzaActions.UPDATE_PIZZA_SUCCESS,
+      pizzaActions.REMOVE_PIZZA_SUCCESS
+    )
+    .pipe(
+      map(pizza => {
+        return new fromRoot.Go({ path: ['products'] });
+      })
+    );
 }
